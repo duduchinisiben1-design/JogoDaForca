795c438b13dde7ef03fa0805cf1c6f27eb1ae5a6
Carlos Eduardo Chini Siben <duduchinisiben1@gmail.com>
2026-03-16 16:52:27 -0300

    Incio do Jogo da forca

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Bem-Vindo ao Jogo Da Forca");
+            Console.WriteLine("---------------------------");
+
+            string? palavraAleatória = EscolherPalavraAlatoria();
+
+            Console.WriteLine(palavraAleatória);
+
+            char[] letrasAcertadas = new char[palavraAleatória.Length];
+
+            for (int caractere = 0; caractere < letrasAcertadas.Length; caractere++)
+            {
+                letrasAcertadas[caractere] = '_';
+            }
+
+            bool jogadorAcertouPalavra = false;
+
+            while (!jogadorAcertouPalavra)
+            {
+                Console.WriteLine(letrasAcertadas);
+
+                Console.WriteLine("Digite uma letra: ");
+                string? strLetra = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(strLetra))
+                {
+                    Console.WriteLine("Digite um caractér válido.");
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("Clique ENTER para prosseguir");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                char letraChute = char.ToUpper(Convert.ToChar(strLetra));
+
+                for (int contador = 0; contador < palavraAleatória.Length; contador++)
+                {
+                    char letraAtual = palavraAleatória[contador];
+
+                    if (letraChute == letraAtual)
+                    {
+                        letrasAcertadas[contador] = letraAtual;
+                    }
+                }
+
+                jogadorAcertouPalavra = palavraAleatória == string.Join("", letrasAcertadas);
+
+            }
+
+            Console.WriteLine("Deseja continuar o jogo? S para continuar e N para sair");
+            string? opcaoDeSaida = Console.ReadLine()?.ToUpper();
+
+            if (opcaoDeSaida != "S")
+            {
+                Console.WriteLine("Você finalizou seu programa, tenha um bom dia!!");
+                break;
+            }
+        }
+
+        static string EscolherPalavraAlatoria()
+        {
+
+            Console.WriteLine("Escolhendo palavra...");
+            string[] palavras = [
+        "ABACATE",
+            "ABACAXI",
+            "ACEROLA",
+            "AÇAÍ",
+            "ARAÇÁ",
+            "ABACATE",
+            "BACABA",
+            "BACURI",
+            "BANANA",
+            "CAJÁ",
+            "CAJU",
+            "CARAMBOLA",
+            "CUPUAÇU",
+            "GRAVIOLA",
+            "GOIABA",
+            "JABUTICABA",
+            "JENIPAPO",
+            "MAÇÃ",
+            "MANGABA",
+            "MANGA",
+            "MARACUJÁ",
+            "MURICI",
+            "PEQUI",
+            "PITANGA",
+            "PITAYA",
+            "SAPOTI",
+            "TANGERINA",
+            "UMBU",
+            "UVA",
+            "UVAIA"
+    ];
+
+            int indiceAleatorio = RandomNumberGenerator.GetInt32(palavras.Length);
+
+
+            string palavraAleatoria = palavras[indiceAleatorio];
+            return palavraAleatoria;
+        }
+
+    }
+}
